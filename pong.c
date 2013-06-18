f57801d614bc0aae7701c8cd94e48dd0fd4dbca0
Ben Davenport-Ray <ben.davenportray@gmail.com>
2013-06-18 09:41:07 -0400

    Added update_ball.
    + Updating ball physics.

--- pong.c
+++ pong.c
@@ -2,12 +2,18 @@
 #include <stdbool.h>
 #include <SDL/SDL.h>
 
+// ** Declarations.
+
+// * Definitions.
 #define PLAYER1 0
 #define PLAYER2 1
-
 #define MAXSCORE 10
 #define LENGTH 20
+#define PADDLE1X 
+#define PADDLE2X
 
+
+// * Game-wide structures.
 bool is_running;
 
 struct ball {
@@ -20,10 +26,7 @@ struct ball {
 struct paddle {
     int y;
     //int length;
-};
-
-struct paddle p1;
-struct paddle p2;
+} p1, p2;
 
 int playerscores[] = {0, 0};
 
@@ -31,7 +34,18 @@ SDL_Surface* screen;
 
 int screensize[] = {800, 600};
 
-void declarewin(int player){
+// ** Function Declarations.
+void declare_win(int player);
+void loop();
+void reset_ball();
+void reset_paddle(struct paddle* p);
+void score(int player);
+void setup_game();
+void update_ball();
+void update_paddle();
+
+// Declare a winner of the game.
+void declare_win(int player){
     // TODO: Add a dialog saying that the player won
     is_running = false;    
 }
@@ -42,6 +56,35 @@ void score(int player)
     if (playerscores[player] >= MAXSCORE) declarewin(player);
 }
 
+void update_ball(){
+    // Check if the ball needs to interact with a paddle or score
+    if (Ball.x >= PADDLE2X){
+        if (Ball.y <= p2.y && Ball.y >= p2.y + LENGTH){
+            Ball.xspeed = -(Ball.xspeed);
+        } else {
+            score(PLAYER1);
+            reset_round();
+            return;
+        }
+
+    } else if (Ball.x <= PADDLE1X){
+        if (ball.y <= p1.y && ball.y >= p1.y + LENGTH){
+            Ball.xspeed = -(Ball.xspeed);
+        } else {
+            score(PLAYER2);
+            reset_round();
+            return;
+        }
+    }
+    // bounce off of the walls if it connects with the top or bottom
+    if (Ball.y >= TOP || Ball.y <= BOTTOM){
+        Ball.yspeed = -(Ball.yspeed);
+    }
+    Ball.x += Ball.xspeed;
+    Ball.y += Ball.yspeed;
+}
+
+
 void reset_ball(){
     Ball.x = 300;
     Ball.y = 400;
@@ -51,6 +94,10 @@ void reset_ball(){
 
 void reset_paddle(struct paddle* p){
     p->y = 290;
+}
+
+void update_paddle(){
+    
 }
 
 void setupGame()
@@ -65,8 +112,6 @@ void loop()
 {
     while(is_running){
         
-        update_paddle(p1);
-        update_paddle(p2);
         update_ball(Ball);
         redraw();
     }
